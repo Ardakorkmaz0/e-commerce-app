9db929024739db5a9dd8c77dbff2aee3cf91899d
ArdaKorkmaz0 <korkmazarda4455@gmail.com>
2026-08-07 14:57:15 +0300

    navbar added now basic site structure is ready

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 app_name = 'ecommerce'
 
 urlpatterns = [
 	path('', views.home, name='home'),
     path('signin/', views.signin, name='signin'),
+    path("signout/", LogoutView.as_view(), name="signout"),
 ]
